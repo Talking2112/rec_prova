a2f0ce19dfe29cfee0cb34b496b7b755aeaf8a39
Aluno SENAI <Aluno@SN-SEMPAT30314.edu.fiesc.com.br>
2025-04-24 10:08:13 -0300

    Encapsulamento de propriedades sensíveis com getters e setters

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 class Personagem {
+    #vida
+    #forca
+    #mana
     constructor(vida, forca, mana){
         this.vida = vida;
         this.forca = forca;
         this.mana = mana
     }
+
     atacar(){
         console.log("Atacando")
     }
@@ -13,7 +17,30 @@ class Personagem {
     usarMagia(){
 
     }
+    get vida (){
+        return this.#vida
+    }
+    set vida (vida){
+        this.#vida = vida
+    }
+    get forca (){
+        return this.#forca
+    }
+    set forca (forca){
+        this.#forca = forca
+    }
+    get mana (){
+        return this.#mana
+    }
+    set mana (mana){
+        this.#mana = mana
+    }
 }
+let novoPersonagem = new Personagem()
+novoPersonagem.vida = 100
+novoPersonagem.forca = 90
+novoPersonagem.mana = 80
+console.log("A vida do seu personagem é", novoPersonagem.vida, ",a força do seu personagem é", novoPersonagem.forca, "e a mana do seu personagem é", novoPersonagem.mana)
 
 class Guerreiro extends Personagem{
     atacar(){
