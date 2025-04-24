c6329f66ed42d39fe6226096e306db4d885eadcc
Aluno SENAI <Aluno@SN-SEMPAT30314.edu.fiesc.com.br>
2025-04-24 08:20:12 -0300

    ataque, defesa, magia

--- script.js
+++ script.js
@@ -1,11 +1,35 @@
 class Guerreiro extends Pesonagem{
+    atacar(){
 
+    }
+    defender(){
+
+    }
+    usarMagia(){
+
+    }
 }
 
 class Mago extends Pesonagem{
-    
+        atacar(){
+
+    }
+    defender(){
+
+    }
+    usarMagia(){
+        
+    }
 }
 
 class Arqueiro extends Pesonagem{
-    
+    atacar(){
+
+    }
+    defender(){
+
+    }
+    usarMagia(){
+        
+    }
 }
